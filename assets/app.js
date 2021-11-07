16ca36fccf98b89cd1a3e190956816fbcf6b4d2d
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-11-07 17:13:04 -0600

    Creado archivo de constantes para almacenar las url de API

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,7 @@
 import { getData } from './js/getData.js';
 import { getContinent } from './js/getContinent.js';
 import { showData } from './js/showData.js';
-
-const API_SERVERS = {
-    America:
-        'http://consultas-distribuidas.levirevilla.com/geolocation-api-main/',
-    Europe: 'http://bdd-act-31.saulgomezc.com/api/',
-    Asia: 'http://api-montse.veracano.com/',
-};
+import { API_SERVERS } from './js/constants.js';
 
 const { continente, ip } = await getContinent();
 const data = await getData(API_SERVERS[continente] || undefined);
---
+++ assets/js/constants.js
@@ -0,0 +1,6 @@
+export const API_SERVERS = {
+    America:
+        'http://consultas-distribuidas.levirevilla.com/geolocation-api-main/',
+    Europe: 'http://bdd-act-31.saulgomezc.com/api/',
+    Asia: 'http://api-montse.veracano.com/',
+};
